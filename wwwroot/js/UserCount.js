0210f7e058700256186716ac7dec7c8a49e8c6d2
khorshedHasanTutul <khasan7890@gmail.com>
2023-10-11 18:54:06 +0600

    Add project files.

---
+++ wwwroot/js/UserCount.js
@@ -0,0 +1,29 @@
+﻿var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+connectionUserCount.on("UpdateTotalViews", (value) => {
+
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    console.log({ newCountSpan }, {value});
+    newCountSpan.innerText = value;
+});
+
+connectionUserCount.on("UpdateTotalUsers", (value) => {
+    var newUserSpan = document.getElementById("totalUsersCounter");
+    newUserSpan.innerText = value;
+});
+
+
+function newWindowLoadedOnCleint() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+function fulFilled() {
+    console.log("Connection To User Hub Successfully.");
+    newWindowLoadedOnCleint();
+}
+
+function rejected() {
+    console.log("Connection Failed");
+}
+
+connectionUserCount.start().then(fulFilled, rejected);
