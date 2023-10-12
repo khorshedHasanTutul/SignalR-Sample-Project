e9c32df72b3fb3e66e98c7fc7d8b0eb010c7cfb3
khorshedHasanTutul <khasan7890@gmail.com>
2023-10-12 18:35:35 +0600

    modifyForSignalR Concept to winform Service

--- Hubs/UserHub.cs
+++ Hubs/UserHub.cs
@@ -6,15 +6,26 @@ namespace SignalRProject.Hubs
     {
         public static int TotalViews { get; set; } = 0;
         public static int TotalUsers { get; set; } = 0;
+        private readonly Dictionary<string , string> UserConnectionMapping = new Dictionary<string, string>();
 
         public override Task OnConnectedAsync()
         {
+            //string userID = Context.User.Identity.Name;
+            //string connectionID = Context.ConnectionId;
+            //UserConnectionMapping[userID] = connectionID;
             TotalUsers++;
             Clients.All.SendAsync("UpdateTotalUsers" , TotalUsers).GetAwaiter().GetResult();    
             return base.OnConnectedAsync();
         }
         public override Task OnDisconnectedAsync(Exception? exception)
         {
+            //string userId = Context.User.Identity.Name;
+
+            //if (UserConnectionMapping.ContainsKey(userId))
+            //{
+            //    UserConnectionMapping.Remove(userId);
+            //}
+
             TotalUsers--;
             Clients.All.SendAsync("UpdateTotalUsers", TotalUsers).GetAwaiter().GetResult();
             return base.OnDisconnectedAsync(exception);
@@ -23,7 +34,16 @@ namespace SignalRProject.Hubs
         public async Task NewWindowLoaded()
         {
             TotalViews++;
-            await Clients.All.SendAsync("UpdateTotalViews" , TotalViews);
+            await Clients.All.SendAsync("UpdateTotalViews",TotalViews);
+        }
+
+        public async void SendMessageToUser(string userId , string message)
+        {
+            if (UserConnectionMapping.ContainsKey(userId))
+            {
+                string connectionId = UserConnectionMapping[userId];
+                await Clients.Client(connectionId).SendAsync("UpdateTotalViews" , message);
+            }
         }
 
     }
